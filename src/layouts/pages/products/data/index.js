6ad32e7e39cd7f8557c15ef0618b159880906afc
M00n-man92 <azraelnumb00@gmail.com>
2023-02-21 11:00:52 +0300

    adding a new products page

---
+++ src/layouts/pages/products/data/index.js
@@ -0,0 +1,78 @@
+export const data = [{
+  img: "https://niddf.com/static/niddf.jpg",
+  title: "PREMIUM ACRYLIC PUTTY",
+  cat: "Painting",
+  price: "500.0 Birr"
+},
+{
+  img: "https://niddf.com/media/product_images/aa.jpg",
+  title: "Melamine MDF",
+  cat: "Material",
+  price: "2000.0 Birr"
+},
+{
+  img: "https://niddf.com/media/product_images/3aa.png",
+  title: "Parquet",
+  cat: "Wall & Floor",
+  price: "800.0 Birr"
+
+},
+{
+  img: "https://niddf.com/media/product_images/2aa.png",
+  title: "Image Handler Locker",
+  cat: "Material",
+  price: "4000.0 Birr"
+
+},
+{
+  img: "https://niddf.com/media/product_images/w1.jpg",
+  title: "Model AF-24",
+  cat: "Furniture",
+  price: "22400.0 Birr"
+
+},
+{
+  img: "https://niddf.com/media/product_images/1a.webp",
+  title: "Bass New Bedroom",
+  cat: "Furniture",
+  price: "10000.0 Birr"
+
+},
+{
+  img: "https://niddf.com/media/product_images/bass_yatak_02.jpg",
+  title: "Charisma",
+  cat: "Furniture",
+  price:"None Birr"
+
+},
+{
+  img: "https://niddf.com/media/product_images/anje4670-800x527-1.jpg",
+  title: "Benchcraft Living Room Traemore Sofa 2740338",
+  cat: "Furniture",
+  price: "None Birr"
+},
+{
+  img: "https://niddf.com/media/product_images/27403-38-35.jpg",
+  title: "Benchcraft Living Room Allmaxx Sofa 2810538",
+  cat: "Furniture",
+  price: "None Birr"
+
+
+},
+{
+  img: "https://niddf.com/media/product_images/28105-38-set-10x8-crop.jpg",
+  title: "Benchcraft Living Room Allmaxx Sofa 2810538",
+  cat: "Furniture",
+  price: "None Birr"
+
+
+},
+{
+  img: "https://niddf.com/static/niddf.jpg",
+  title: "Benchcraft Living Room Allmaxx Sofa 2810538",
+  cat: "Furniture",
+  price: "None Birr"
+
+
+},
+]
